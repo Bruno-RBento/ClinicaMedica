f725450353b41c19094729b993445a9d502b07cd
Bruno <bentobruno88@gmail.com>
2022-12-23 15:36:53 +0000

    add core project

--- src/Medico/Especialidade.java
+++ src/Medico/Especialidade.java
@@ -1,18 +1,34 @@
 package Medico;
 
 public enum Especialidade {
-    CARDIOLOGIA,
-    ANESTESIOLOGIA,
-    DERMATOLOGIA,
-    ENDOCRINOLOGIA,
-    GASTROENTEROLOGIA,
-    PNEUMOLOGIA,
-    OBSTERICIA,
-    PSIQUIATRIA,
-    CIRUGIA_GERAL,
-    MEDICINA_TRABALHO,
-    MEDICINA_DESPORTIVA,
-    MEDICINA_FISICA_E_REABILITCAO,
-    MEDICINA_GERAL_E_FAMILIAR
+    CARDIOLOGIA(2000,50),
+    ANESTESIOLOGIA(3000,50),
+    DERMATOLOGIA(2000,50),
+    ENDOCRINOLOGIA(2000,50),
+    GASTROENTEROLOGIA(2000,50),
+    PNEUMOLOGIA(2000,50),
+    OBSTERICIA(2000,50),
+    PSIQUIATRIA(1500,50),
+    CIRUGIA_GERAL(3500,50),
+    MEDICINA_TRABALHO(1500,50),
+    MEDICINA_DESPORTIVA(1500,50),
+    MEDICINA_FISICA_E_REABILITCAO(1500,50),
+    MEDICINA_GERAL_E_FAMILIAR(1500,50);
 
+    private int salario;
+    private int custoConsulta;
+
+    Especialidade(int salario, int custoConsulta) {
+        this.salario = salario;
+        this.custoConsulta = custoConsulta;
+    }
+
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public int getCustoConsulta() {
+        return custoConsulta;
+    }
 }
