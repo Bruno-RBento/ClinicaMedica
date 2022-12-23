171ac484f171c8e6faa4885395adc47e001a99e9
Diogo Capela <52971538+hotkingkeys@users.noreply.github.com>
2022-12-23 14:58:54 +0000

    Add files via upload
    Atualização do consultorio

--- src/Consultorio/FuncConsultorio.java
+++ src/Consultorio/FuncConsultorio.java
@@ -1,4 +1,119 @@
-package Consultorio;
+package consultorio;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import medico.Especialidade;
 
 public class FuncConsultorio {
+	private static Scanner ler = new Scanner(System.in);
+	private static File file1 = new File("C:\\Users\\diogo\\Desktop\\UBI\\2oAno\\1º Semestre\\Programação Orientada a Objetos\\Trabalho de Grupo\\ClinicaMedica-main\\src\\src\\consultorio\\salas.dat"); 
+	
+	public static void adicionarConsultorio(ArrayList<Consultorio> salas) {
+		System.out.println("Qual a especialidade do Consultorio?");
+		Especialidade esp = list(); //escolhe uma opção da lista de especialidades
+		System.out.println("Qual as medidas do consultorio no formato - (width height)");
+		String medidas = ler.nextLine();
+		String lados[] = medidas.split(" ");
+		
+		Consultorio c = new Consultorio(esp, Integer.parseInt(lados[0]), Integer.parseInt(lados[1]));
+		salas.add(c);
+		
+		try {
+			ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(file1));
+			os.writeInt(Consultorio.getUltNumero());
+			os.writeObject(salas);
+			os.flush();
+			os.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void getSala(ArrayList<Consultorio> salas) {
+		System.out.println("Qual o número do Consultorio?");
+		int num = ler.nextInt();
+		Consultorio x = new Consultorio();
+		
+		for(int i = 0; i < salas.size(); i++) {
+			if(salas.get(i).getNum() == num) {
+				x = salas.get(i).clone();
+				break;
+			}
+		}
+		
+		System.out.println("Consultorio nº " + x.getNum()
+							+ "\nEspecialidade: " + x.getEspecialidade()
+							+ "\nArea: " + x.getArea());
+	}
+	
+	public static void altEsp(ArrayList<Consultorio> salas) {
+		System.out.println("Qual o número do Consultorio?");
+		int num = ler.nextInt();
+		ler.nextLine(); //ler o "\n"
+		for(int i = 0; i < salas.size(); i++) {
+			if(salas.get(i).getNum() == num) {
+				System.out.println("Qual a nova especialidade?");
+				salas.get(i).setEspecialidade(list());
+				break;
+			}
+		}
+		
+		try {
+			ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(file1));
+			os.writeInt(Consultorio.getUltNumero());
+			os.writeObject(salas);
+			os.flush();
+			os.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void areaT(ArrayList<Consultorio> salas) {
+		int areaT = 0;
+		
+		for(Consultorio i : salas) {
+			areaT += i.getArea();
+		}
+		
+		System.out.println("Area Total: " + areaT + " m2");
+	}
+	
+	private static Especialidade list() {
+		System.out.println("1 - CARDIOLOGIA"
+        				+ "\n2 - ANESTESIOLOGIA"
+        				+ "\n3 - DERMATOLOGIA"
+        				+ "\n4 - ENDOCRINOLOGIA"
+        				+ "\n5 - GASTROENTEROLOGIA"
+        				+ "\n6 - PNEUMOLOGIA"
+        				+ "\n7 - OBSTERICIA"
+        				+ "\n8 - PSIQUIATRIA"
+        				+ "\n9 - CIRUGIA_GERAL"
+        				+ "\n10 - MEDICINA_TRABALHO"
+        				+ "\n11 - MEDICINA_DESPORTIVA"
+        				+ "\n12 - MEDICINA_FISICA_E_REABILITCAO"
+        				+ "\n13 - MEDICINA_GERAL_E_FAMILIAR");
+        int opcao = ler.nextInt();
+        ler.nextLine();
+
+        switch (opcao){
+            case 1: return Especialidade.CARDIOLOGIA;
+            case 2: return Especialidade.ANESTESIOLOGIA;
+            case 3: return Especialidade.DERMATOLOGIA;
+            case 4: return Especialidade.ENDOCRINOLOGIA;
+            case 5: return Especialidade.GASTROENTEROLOGIA;
+            case 6: return Especialidade.PNEUMOLOGIA;
+            case 7: return Especialidade.OBSTERICIA;
+            case 8: return Especialidade.PSIQUIATRIA;
+            case 9: return Especialidade.CIRUGIA_GERAL;
+            case 10: return Especialidade.MEDICINA_TRABALHO;
+            case 11: return Especialidade.MEDICINA_DESPORTIVA;
+            case 12: return Especialidade.MEDICINA_FISICA_E_REABILITCAO;
+            case 13: return Especialidade.MEDICINA_GERAL_E_FAMILIAR;
+            default:
+                throw new IllegalStateException("Unexpected value: " + opcao);
+        }
+	}
+	
 }
